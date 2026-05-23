f9aff147d8808b19337060d07108e9e68828bc16
Vishwanath-B-M <vishwanath@Vishwanaths-MacBook-Air.local>
2026-05-23 17:58:31 +0530

    added

--- src/controllers/account.controller.js
+++ src/controllers/account.controller.js
@@ -12,4 +12,31 @@ async function createAccount(req,res) {
     })
     
 }
-module.exports={createAccount}
+async function getAllaccounts(req,res) {
+    const user=req.user
+    console.log(user)
+    const accounts=await accountmodel.find({user:req.user._id})
+    res.status(200).json({
+        message:"accounts",
+        accounts
+    })
+    
+}
+async function getBalance(req,res) {
+    const {accountid}=req.body
+    const useraccountisthere=await accountmodel.findOne({
+        _id:accountid,user:req.user._id})
+        if(!useraccountisthere){
+            return res.status(400).json({
+                message:"you can't bro"
+            })
+        }
+        const balance=await accountmodel.balance()
+        res.status(200).json({
+            message:"balance is here",
+            accountid:account._id,
+            balance:balance
+        })
+    
+}
+module.exports={createAccount,getAllaccounts,getBalance}
--- src/routers/account.router.js
+++ src/routers/account.router.js
@@ -3,6 +3,8 @@ const accountController=require("../controllers/account.controller")
 const authMiddleware=require("../middlewares/auth.middleware")
 const router=express.Router();
 router.post("/create",authMiddleware.userCheck,accountController.createAccount)
+router.get("/accounts",authMiddleware.userCheck,accountController.getAllaccounts)
+router.get("/accounts/:accountid",authMiddleware.userCheck,accountController.getBalance)
 
 
 module.exports=router;
